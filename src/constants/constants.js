1cae976118b6430b8bf3d008ab24e9e23b55e0ca
Misty-Dev <markojau@gmail.com>
2022-02-17 13:20:25 -0300

    Projects Box corrections

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,17 @@ export const projects = [
 		id: 0,
 	},
 	{
-		title: 'Hypebeast NFT Collection',
+		title: 'CrowdCoin Dapp',
+		description:
+			'CrowdCoin is a dapp inspired by the famous website Kickstarter. Using Solidity Smart contracts, you can verify that the people getting funded are not doing irresponsible things unrelated to the project.',
+		image: '/images/3.jpg',
+		tags: ['React', 'Solidity', 'TDD'],
+		source: 'https://google.com',
+		visit: 'https://github.com/Misty-Dev/kickstart',
+		id: 2,
+	},
+	{
+		title: 'Hypebeast NFTs',
 		description:
 			'NFT Collection made for 72dpi. Exotic animals becoming the Hypebeasts they truly are.',
 		image: '/images/2.png',
@@ -19,20 +29,11 @@ export const projects = [
 		visit: 'https://github.com/Misty-Dev/hypebeast-sc',
 		id: 1,
 	},
-	{
-		title: 'CrowdCoin Dapp',
-		description:
-			'CrowdCoin is a dapp inspired by the famous website Kickstarter, improving on concept they created. Using Solidity Smart contracts, you can verify that the people getting funded are not doing irresponsible things unrelated to the project by using a voting system in which the investors are in control of whether transactions go through or not.',
-		image: '/images/3.jpg',
-		tags: ['React', 'Solidity', 'TDD'],
-		source: 'https://google.com',
-		visit: 'https://github.com/Misty-Dev/kickstart',
-		id: 2,
-	},
+
 	{
 		title: 'Web3 Sisters',
 		description:
-			'This is a landing page for the Web3 Sisters (Yet to be deployed)',
+			'This is a landing page for the Web3 Sisters. NFT agents for the artists of the future. (Yet to be deployed)',
 		image: '/images/4.jpg',
 		tags: ['React'],
 		source: 'https://google.com',
